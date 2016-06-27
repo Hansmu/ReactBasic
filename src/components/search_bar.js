c09c42f595665f6a28ca9c4f5e8c02bdcab51983
hansmu <hans.miikael@gmail.com>
2016-06-27 14:32:47 +0300

    State initialization.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'; //Need React whenever you use JSX. {} 
 //Functional component is just a function. Class component is used when you want internal record keeping.
 //Should use a functional component by default and only refactor if it looks like you're going to need a class based component.
 export default class SearchBar extends Component {
+    constructor(props) { //All JS classes have a constructor. Automatically called when a new instance of the class is created.
+        super(props); //Call parent class constructor.
+        this.state = { term: '' }; //When we use state, we initialize it and set it to this.state.
+    }
+
     render() { //Must always have a render method in a component.
         return <input onChange={this.onInputChange} />; //When the input changes, then run the method onInputChange. Whenever we use JS variables we use {}.
         //return <input onChange={ (event)=>console.log(event.target.value) } />;
