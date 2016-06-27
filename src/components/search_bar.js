eb00de202438cee1a79597a8bfb519e2953cebf0
hansmu <hans.miikael@gmail.com>
2016-06-27 14:43:51 +0300

    Controlled component.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,13 +8,16 @@ export default class SearchBar extends Component {
     }
 
     render() { //Must always have a render method in a component.
-        return <input onChange={this.onInputChange} />; //When the input changes, then run the method onInputChange. Whenever we use JS variables we use {}.
-        //return <input onChange={ (event)=>console.log(event.target.value) } />;
+        return <input //this.setState causes the component to re-render. When it re-renders, the value of the input is set to the new value of this.state.term.
+            value={ this.state.term } //Controlled component. A controlled component has its value set by state. Its value only ever changes when the state changes.
+            onChange={ this.onInputChange } />; //When the input changes, then run the method onInputChange. Whenever we use JS variables we use {}.
+        //return <input onChange={ (event)=>console.log(event.target.value) } />; Define the function inline.
     }
 
     onInputChange(event) { //All browser events that get triggered by elements like input or such. Whenever we add an event handler, they're always called with an event object.
         // We can name it whatever we want. Event object describes the event that happened
-        console.log(event.target.value);
+        this.setState({ term: event.target.value }); //Always use this.setState to change state. Causes the component to re-render every time the state changes.
+        //Whenever we want to update our component, think of state.
     }
 }
 
