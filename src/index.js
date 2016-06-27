a489ceafe7a760e7d82a3ee7a4efda4a6d3d813d
hansmu <hans.miikael@gmail.com>
2016-06-27 14:59:16 +0300

    Refactor to class component. Video search.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 //Every file that we write is separated from other code that we write, so we have to use imports to get access to other files.
-import React from 'react'; //Knows how to work with React components.
+import React, { Component } from 'react'; //Knows how to work with React components.
 import ReactDOM from 'react-dom'; //To render the elements to the actual DOM.
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = "AIzaSyDkidiQEgNxblRS5oJvfud5Xf3T9M2kaYA";
 
+//React has downwards dataflow. It means that only the most parent component should be in charge of fetching the data.
+
 //Create a new component. This component should produce some HTML.
-const App = () => { // const is ES6 syntax. A constant. => Fat arrow ES6 syntax to declare a function.
-    return (
+class App extends Component { // const is ES6 syntax. A constant. => Fat arrow ES6 syntax to declare a function.
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos }); //videos: videos can be condensed to that because the names are the same.
+        });
+    }
+
+    return() {
         <div>
             <SearchBar />
         </div>
-    ); //JSX - lets us write HTML looking code that's really just Javascript. JSX needs to be translated, can't be understood normally by a browser.
+    }; //JSX - lets us write HTML looking code that's really just Javascript. JSX needs to be translated, can't be understood normally by a browser.
     //Have parentheses when you have a multiline element.
     //Without JSX, the code would be a lot less readable and lengthier.
 }
